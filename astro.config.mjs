a87e9444a0eebec49cdbc6b6c640ced9efc17bb5
RINGENBACH-ALEX <alex.ringenbach@edu.univ-fcomte.fr>
2025-03-14 09:21:49 +0100

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 
+import netlify from '@astrojs/netlify';
+
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
-    experimental: {
-        svg: true,
-    },
-    output: 'server'
+  integrations: [tailwind()],
+
+  experimental: {
+      svg: true,
+  },
+
+  output: 'server',
+  adapter: netlify()
 });
