a96de7c758ad3dbebddf1faba9f1dab35e4f7860
RINGENBACH-ALEX <alex.ringenbach@edu.univ-fcomte.fr>
2025-03-14 09:09:43 +0100

    bas.css + tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./src/css/Base.css', // Ajoutez ce chemin pour inclure votre fichier CSS personnalisé
+	],
 	theme: {
-		extend: {},
+		extend: {
+			// Ajoutez ici vos styles personnalisés si nécessaire
+			colors: {
+				'custom-green': '#10B981',
+				'custom-orange': '#F97316',
+			},
+			fontSize: {
+				'6xl': '4rem',
+				'8xl': '6rem',
+			},
+			spacing: {
+				'100': '25rem',
+			},
+		},
 	},
 	plugins: [],
 }
